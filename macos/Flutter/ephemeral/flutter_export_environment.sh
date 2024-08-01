b909fa9db35453dda8a180888c6b9a06b93453d8
leesehwi <leesehwi@minigate.net>
2024-08-01 11:18:28 +0900

    코드정리1

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sehwing/Desktop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sehwing/Desktop/modu_messenger_firebase"
+export "FLUTTER_ROOT=/Users/minigate/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/minigate/AndroidStudioProjects/modu_messanger"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
